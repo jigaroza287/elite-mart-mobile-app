c846ccb304426387d6542a955e50368afa2e2be2
Jigar Oza <14957546+jigaroza287@users.noreply.github.com>
2024-12-05 23:58:51 +0530

    feat: Added generic horizontal list component

---
+++ src/components/horizontalList/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {FlatList, FlatListProps, ListRenderItem, ViewStyle} from 'react-native';
+import style from './style';
+
+interface HorizontalListProps<T> extends Omit<FlatListProps<T>, 'renderItem'> {
+  renderItem: (item: T) => React.ReactElement | null;
+  contentContainerStyle?: ViewStyle;
+}
+
+const HorizontalList = <T,>({
+  data,
+  renderItem,
+  contentContainerStyle,
+  ...props
+}: HorizontalListProps<T>) => {
+  const renderListItem: ListRenderItem<T> = ({item}) => renderItem(item);
+
+  return (
+    <FlatList
+      data={data}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      renderItem={renderListItem}
+      keyExtractor={(item, index) => index.toString()}
+      contentContainerStyle={[style.listContainer, contentContainerStyle]}
+      {...props}
+    />
+  );
+};
+
+export default HorizontalList;
---
+++ src/components/horizontalList/style.ts
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+import {spacing} from '../../theme';
+
+export default StyleSheet.create({
+  listContainer: {
+    paddingHorizontal: spacing.xSmall,
+  },
+});
