14cc5207b4b8eac89e109bae1c43bf1abb16d640
Jigar Oza <14957546+jigaroza287@users.noreply.github.com>
2024-12-16 23:54:29 +0530

    feat: Added bottom sheet common component

---
+++ src/components/bottomSheet/index.tsx
@@ -0,0 +1,44 @@
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import style from './style';
+
+export type ReusableBottomSheetRef = {
+  openBottomSheet: () => void;
+  closeBottomSheet: () => void;
+};
+
+type ReusableBottomSheetProps = {
+  children: React.ReactNode;
+};
+
+const ReusableBottomSheet = forwardRef<
+  ReusableBottomSheetRef,
+  ReusableBottomSheetProps
+>(({ children }, ref) => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  useImperativeHandle(ref, () => ({
+    openBottomSheet: () => {
+      console.log('bottomSheetRef.current: \n');
+      console.log(bottomSheetRef.current);
+      bottomSheetRef.current?.expand();
+    },
+    closeBottomSheet: () => {
+      bottomSheetRef.current?.close();
+    },
+  }));
+
+  return (
+    <BottomSheet
+      ref={bottomSheetRef}
+      index={-1}
+      backgroundStyle={style.sheetBackground}
+      enableContentPanningGesture={false}>
+      <BottomSheetView style={style.contentContainer}>
+        {children}
+      </BottomSheetView>
+    </BottomSheet>
+  );
+});
+
+export default ReusableBottomSheet;
---
+++ src/components/bottomSheet/style.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { colors, spacing } from '../../theme';
+
+export default StyleSheet.create({
+  sheetBackground: {
+    backgroundColor: colors.light,
+  },
+  contentContainer: {
+    padding: spacing.medium,
+  },
+});
